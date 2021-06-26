fe2827ce4e629a5980af1d5fbba620d678a7a71a
santiyago <santiyagoburcart@gmail.com>
2021-06-26 07:34:56 +0430

    edir fail test auth

--- authentication/tests/test_models.py
+++ authentication/tests/test_models.py
@@ -2,15 +2,10 @@ from utils.setup_test import TestSetup
 from authentication.models import User
 
 
-
-
-
-
-
-
-class TestMOdels(TestSetup):
+class TestModel(TestSetup):
 
     def test_should_create_user(self):
+
         user = self.create_test_user()
 
         self.assertEqual(str(user), user.email)
